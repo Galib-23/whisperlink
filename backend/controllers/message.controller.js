4e42b90c1966e937fedf36fc626f2c5195c45295
Galib-23 <aagalib2323@gmail.com>
2024-08-04 00:32:21 +0600

    create getMessages controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -29,9 +29,35 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
+    // -- This runs one after another
+    // await conversation.save();
+    // await newMessage.save();
+
+    // This runs parallely
+    await Promise.all([conversation.save(), newMessage.save()]);
+
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+        participants: {
+            $all: [ senderId, userToChatId]
+        },
+    }).populate("messages");
+
+    res.status(200).json(conversation.messages);
+
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
