715357fd56fcd4a54005d44a8b2bd9db450c89c3
Galib-23 <aagalib2323@gmail.com>
2024-08-06 19:47:19 +0600

    create message skeleton and fetch messages

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -49,13 +49,18 @@ export const getMessages = async (req, res) => {
     const senderId = req.user._id;
 
     const conversation = await Conversation.findOne({
-        participants: {
-            $all: [ senderId, userToChatId]
-        },
+      participants: {
+        $all: [senderId, userToChatId],
+      },
     }).populate("messages");
 
-    res.status(200).json(conversation.messages);
+    let messages = [];
 
+    if (conversation) {
+      messages = [...conversation.messages];
+    }
+
+    res.status(200).json(messages);
   } catch (error) {
     console.log("Error in getMessages controller: ", error.message);
     res.status(500).json({ error: "Internal Server Error" });
