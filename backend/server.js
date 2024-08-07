e1802c6158a60625cbb47af4fe845fee32570c7a
Galib-23 <aagalib2323@gmail.com>
2024-08-07 16:18:31 +0600

    initial socket setup for backend

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js"
 import messageRoutes from "./routes/message.routes.js";
 import connectToDB from "./db/connectToDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const port = process.env.PORT || 5000;
 
 dotenv.config();
@@ -21,7 +21,7 @@ app.use("/api/users", userRoutes)
 
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, async () => {
+server.listen(port, async () => {
     connectToDB();
     console.log(`Whisperlink server listening on port ${port}!`)
 });
